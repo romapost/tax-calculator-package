576bb4d9a7e550d2b47ebe6997bc53b54c70a261
romapost <roman.postovalov@gmail.com>
2021-01-06 11:45:12 +0100

    Renamed tests/CalculationTest.php

--- tests/CalculationTest.php
+++ tests/CalculationTest.php
@@ -10,10 +10,16 @@ use Realforce\Finance\Calculation;
 use Realforce\Finance\Child;
 use Realforce\Finance\Salary;
 
-class CalculatorTest extends TestCase
+class CalculationTest extends TestCase
 {
+    /**
+     * @var Calculation
+     */
+    private $calculator;
+
     protected function setUp(): void
     {
+        $this->calculator = new Calculation();
     }
 
     protected function tearDown(): void
@@ -22,15 +28,19 @@ class CalculatorTest extends TestCase
 
     public function testCalc(): void
     {
-        $calculator = new Calculation();
 
         $person = new Adult('Alice', 26, [new Child('Child1', 3), new Child('Child2', 5)]);
-        $salary = $calculator->setPerson($person)->calc(new Salary(6000));
+        $salary = $this->calculator->setPerson($person)->calc(new Salary(6000));
+
+        $this->assertIsFloat($salary);
+        $this->assertEqualsWithDelta(4800, $salary, 0.001);
 
         dump($salary);
 
         $person = new Adult('Bob', 52, []);
-        $salary = $calculator->setPerson($person)->calc(new Salary(4000));
+        $salary = $this->calculator->setPerson($person)->calc(new Salary(4000));
+        $this->assertIsFloat($salary);
+        $this->assertEqualsWithDelta(3424, $salary,0.001);
 
         dump($salary);
 
@@ -39,7 +49,9 @@ class CalculatorTest extends TestCase
             new Child('Child2', 5),
             new Child('Child2', 16),
         ]);
-        $salary = $calculator->setPerson($person)->calc(new Salary(5000));
+        $salary = $this->calculator->setPerson($person)->calc(new Salary(5000));
+        $this->assertIsFloat($salary);
+        $this->assertEqualsWithDelta(4020, $salary,0.001);
 
         dump($salary);
     }
