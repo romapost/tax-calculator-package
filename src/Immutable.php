d607c5b27c7ea73bbbbec731464f1c4f722e0602
romapost <roman.postovalov@gmail.com>
2021-01-25 22:41:25 +0100

    Fixed testImmutable

--- src/Immutable.php
+++ src/Immutable.php
@@ -10,24 +10,41 @@ namespace Realforce\Finance;
  */
 final class Immutable
 {
+    /**
+     * @var string
+     */
     private string $property;
 
     /**
      * Immutable constructor.
+     * @param string $property
      */
     private function __construct(string $property)
     {
         $this->property = $property;
     }
 
+    public function __set($name, $value)
+    {
+        throw new \BadMethodCallException("Cannot add new property \$$name to instance of " . __CLASS__);
+    }
 
+    private function __clone()
+    {
+        throw new \BadMethodCallException("Cannot clone object of instance " . __CLASS__);
+    }
+
+    /**
+     * @return string
+     */
     public function getProperty(): string
     {
         return $this->property;
     }
 
     /**
-     * @return static
+     * @param string $property
+     * @return Immutable
      */
     public static function withProperty(string $property): self
     {
--- tests/ImmutableTest.php
+++ tests/ImmutableTest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
+use Realforce\Finance\Client;
 use Realforce\Finance\Immutable;
+use Realforce\Finance\Invoices\InvoicePdfGenerator;
 
 class ImmutableTest extends TestCase
 {
@@ -13,5 +15,18 @@ class ImmutableTest extends TestCase
     {
         $class = Immutable::withProperty('test');
         $this->assertSame('test', $class->getProperty());
+
+        try {
+            // Try setter property
+            $class->property = 'test';
+            $this->exceptException(\BadMethodCallException::class);
+
+            // Try clone immutable object
+            $clone = clone $class;
+            $this->exceptException(\BadMethodCallException::class);
+
+        } catch (\BadMethodCallException $exception) {
+
+        }
     }
 }
